6daa0ce3b5a890059c61356d132ab6718ab012ca
Sean Soriano <guitarisean@gmail.com>
2026-03-02 05:22:10 +0800

    feat:
    follow up commit message from last commit:
    
    - add mission partners section
    -  add strategic acquisitions (what's new section)
    - add overlay helper
    
    current commit:
    - remove lines importing deleted npm packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Theme } from '@radix-ui/themes'
+
 import { Routes } from '@generouted/react-router'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-         <Routes /> 
+        <Routes />
     </StrictMode>,
 )
