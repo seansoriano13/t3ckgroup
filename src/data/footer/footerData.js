a8609c1c945ba71a05d4e9fae9ec1249e95551f3
Sean Soriano <guitarisean@gmail.com>
2026-03-06 10:31:47 +0800

    add: footerData

---
+++ src/data/footer/footerData.js
@@ -0,0 +1,66 @@
+const DEFAULT_LOGO = "main-logo-w-text.png";
+const DEFAULT_ADDRESS =
+  "No. 8 Lt. Amb. F. Neri Ave. Blue Mountain Subd. Sta. Cruz, Antipolo City";
+
+export const footerData = {
+  main: {
+    logo: DEFAULT_LOGO,
+    description:
+      "Engineered for survival. Providing elite tactical, rescue, and infrastructure solutions for national defense and local resilience.",
+    cta: {
+      primary: "REQUEST A QUOTE",
+      secondary: "CONTACT T3CK",
+    },
+    copyright: "@ 2026 T3CKGROUP. ALL RIGHTS RESERVED.",
+    address: DEFAULT_ADDRESS,
+    contacts: [
+      {
+        countryCode: "+63",
+        number: "9063678910",
+        network: "Globe",
+      },
+      {
+        countryCode: "+63",
+        number: "9063678910",
+        network: "Smart",
+      },
+      {
+        countryCode: "+63",
+        number: "286362892",
+        network: "Landline",
+      },
+    ],
+    email: "informe@t3ckgroup.com",
+    links: [
+      {
+        title: "T3CKGROUP OF COMPANIES",
+        links: [
+          {
+            label: "TGGT OPC",
+            href: "/tggt-opc",
+          },
+          {
+            label: "T3CK TRADING",
+            href: "/t3cktrading",
+          },
+          {
+            label: "TCA CBS",
+            href: "/tca-cbs",
+          },
+          {
+            label: "TG ENT",
+            href: "/tg-ent",
+          },
+          {
+            label: "DII",
+            href: "/dii",
+          },
+          {
+            label: "EOOC",
+            href: "",
+          },
+        ],
+      },
+    ],
+  },
+};
