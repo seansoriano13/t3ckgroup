8b7ff9c509c181069eeeae1c5539164ad3506c63
Sean Soriano <guitarisean@gmail.com>
2026-03-06 10:26:12 +0800

    feat: fieldOperationsCards hover feature

--- src/components/FieldOperationsCards.jsx
+++ src/components/FieldOperationsCards.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
 import { fieldOperations } from "../data/fieldOps";
+import SecondaryButton from "../components/SecondaryButton";
 
 function FieldOperationsCards() {
   const [selectedId, setSelectedId] = useState(null);
 
-  const handleSelect = (id) => {
+  const handleMouseEnter = (id) => {
     setSelectedId(id);
+  };
+
+  const handleMouseLeave = () => {
+    setSelectedId(null);
   };
 
   return (
     <div className="flex gap-6">
       {fieldOperations.map((fieldOps, i) => {
-        const isSelected = fieldOps.id === selectedId;
-
+        const isSelected = selectedId === fieldOps.id;
         return (
           <div
             key={i}
-            onClick={() => handleSelect(fieldOps.id)}
-            className={`cursor-pointer bg-gray-4 border-b-3  ${isSelected ? "border-red-6 opacity-100" : "border-gray-a6 opacity-70"} `}
+            onMouseEnter={() => handleMouseEnter(fieldOps.id)}
+            onMouseLeave={handleMouseLeave}
+            className={`bg-gray-4 border-b-3  ${isSelected ? "border-red-6 " : "border-gray-a6 "} transition-all`}
           >
-            <img
-              className="h-80 w-full opacity-80 object-cover"
-              src={fieldOps.image}
-              alt=""
-            />
+            <div className="relative transition-colors bg-gray-1">
+              <img
+                className={` transition-opacity h-80 w-full ${isSelected ? "opacity-40" : "opacity-80"} object-cover`}
+                src={fieldOps.image}
+                alt=""
+              />
+              {isSelected && (
+                <div className="absolute-center z-20">
+                  <SecondaryButton label={"VIEW"} />
+                </div>
+              )}
+            </div>
+
             <div className="py-8 px-6">
               <p className="text-xs text-red-8">{fieldOps.dateRange}</p>
               <p className="text-lg">{fieldOps.title}</p>
