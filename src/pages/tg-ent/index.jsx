421b26895847004c3278e2758267f01eeb1db15d
Sean Soriano <guitarisean@gmail.com>
2026-03-25 15:53:28 +0800

    feat(tg-ent): implement ProductSpotlight and GetInTouch layouts

--- src/pages/tg-ent/index.jsx
+++ src/pages/tg-ent/index.jsx
@@ -1,17 +1,23 @@
 import { useRef } from "react";
 import Accordion from "../../components/Accordion";
 import HeroSection from "../../components/HeroSection";
+import ProductSpotlight from "../../components/ProductSpotlight";
+import GetInTouch from "../../components/GetInTouch";
+import { productSpotlightData } from "../../data/productSpotlightData";
 
 function TgEnt() {
   const accordionRef = useRef(null);
   return (
     <>
       <HeroSection
-        upperText="UNIFIED COMMAND"
-        lowerText="BECAUSE SECONDS SAVE LIVES"
-        src="/videos/tg-ent-hero.mp4"
+        scrollToRef={accordionRef}
+        upperText="CONSUMER GRADE"
+        lowerText="TACTICAL SUPPLY"
+        src={""}
       />
+      <ProductSpotlight data={productSpotlightData.tgEnt} />
       <Accordion forwardRef={accordionRef} />
+      <GetInTouch />
     </>
   );
 }
