8f309ac7172e75237d30b307b98a1d767b6254dc
Sean Soriano <guitarisean@gmail.com>
2026-03-25 15:53:27 +0800

    feat(tca-cbs): replace ProductSpotlight with ServicesSlider for capabilities overview

--- src/pages/tca-cbs/index.jsx
+++ src/pages/tca-cbs/index.jsx
@@ -1,17 +1,23 @@
 import { useRef } from "react";
 import Accordion from "../../components/Accordion";
 import HeroSection from "../../components/HeroSection";
+import ServicesSlider from "../../components/ServicesSlider";
+import GetInTouch from "../../components/GetInTouch";
+import { tcaServicesData } from "../../data/tcaServicesData";
 
 function TcaCbs() {
   const accordionRef = useRef(null);
   return (
     <>
       <HeroSection
-        upperText="BUILDING EXPERIENCES"
-        lowerText="ENGINEERED TO LAST"
-        src="/videos/tca-cbs-hero.mp4"
+        scrollToRef={accordionRef}
+        upperText="HEAVY ENGINEERING"
+        lowerText="INFRASTRUCTURE"
+        src={""}
       />
+      <ServicesSlider data={tcaServicesData} />
       <Accordion forwardRef={accordionRef} />
+      <GetInTouch />
     </>
   );
 }
