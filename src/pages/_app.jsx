c2f09ddb14efcaa209462acb04fa07eae6bd08ff
Sean Soriano <guitarisean@gmail.com>
2026-03-25 15:53:27 +0800

    feat(_app): wrap application layout in PageTransition and add ScrollRestoration

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration, useLocation } from "react-router";
 import Navbar from "../components/Navbar";
 import "./noisy.css";
 import Footer from "../components/Footer";
+import PageTransition from "../components/PageTransition";
 
 function _layout() {
+  const location = useLocation();
   return (
     <>
+      <ScrollRestoration />
       <Navbar />
-      <Outlet />
+      <PageTransition key={location.pathname}>
+        <Outlet />
+      </PageTransition>
       <Footer />
     </>
   );
